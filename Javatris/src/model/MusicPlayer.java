559d648a7fe9f2c96e5b4382efd9b9bfb7af6209
Tobias <Tobias@Tobias-Dator>
2020-02-19 18:24:21 +0100

    MusicPlayer mer test och små ändringar

--- Javatris/src/model/MusicPlayer.java
+++ Javatris/src/model/MusicPlayer.java
@@ -27,15 +27,20 @@ public class MusicPlayer {
 		
 		//playMusic4("G:\\firefox\\eclipse object ori\\Javatris2\\Javatris\\src\\songs\\Tetris99 Game Theme.wav");	Lokal dator
 		//playMusic4("Javatris\\\\src\\\\songs\\\\Tetris99 Game Theme.wav"); // Vet inte varför det har blivit fler \\ det var inte fyra från början
-		//playMusic("Javatris/src/songs/Tetris99 Game Theme.wav"); // Denna funkar också		
-		playMusic("\\src\\songs\\Tetris Game Theme.wav");
+		
+		
+		//playMusic("Javatris/src/songs/Tetris99 Game Theme.wav");  // variant 1	
+		playMusic("\\src\\songs\\Tetris Game Theme.wav");			// variant 2	
 	}
 	
 	// funkar
 	public void playMusic(String fp) {
-//		File audioFile = new File(fp);
-		 Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
-		 File audioFile = new File(path + fp);
+		
+//		File audioFile = new File(fp);	// variant 1	
+		
+		
+		 Path path = FileSystems.getDefault().getPath("").toAbsolutePath(); // variant 2	
+		 File audioFile = new File(path + fp);								// variant 2	
 	
 		try {
 								
@@ -76,7 +81,7 @@ public class MusicPlayer {
 		}
 	}
 	
-	public float getVolume() {
+	private float getVolume() {
 		if(fileLoaded) {
 	    
 			FloatControl gainControl = (FloatControl) audioClip.getControl(FloatControl.Type.MASTER_GAIN);        
@@ -103,10 +108,17 @@ public class MusicPlayer {
 	public void incVolume() {
 		if(fileLoaded) {
 			float volume = getVolume();
-			if(volume < 1.0f) {
-				volume += steps;		
+			volume += steps;
+			
+			if(volume < 1.0f) {						
 				volume = (float)Math.round(volume*100)/100;
 				System.out.println(volume);
+				FloatControl gainControl = (FloatControl) audioClip.getControl(FloatControl.Type.MASTER_GAIN);        
+			    gainControl.setValue(20f * (float) Math.log10(volume));
+			}
+			else {
+				volume = 1f;
+				
 				FloatControl gainControl = (FloatControl) audioClip.getControl(FloatControl.Type.MASTER_GAIN);        
 			    gainControl.setValue(20f * (float) Math.log10(volume));
 			}
@@ -117,10 +129,17 @@ public class MusicPlayer {
 	public void decVolume() {
 		if(fileLoaded) {
 			float volume = getVolume();
-			if(volume > 0.0f) {
-				volume -= steps;
+			volume -= steps;
+			
+			if(volume > 0.0f) {				
 				volume = (float) Math.round(volume*100)/100;
 				System.out.println(volume);
+				
+				FloatControl gainControl = (FloatControl) audioClip.getControl(FloatControl.Type.MASTER_GAIN);        
+			    gainControl.setValue(20f * (float) Math.log10(volume));
+			}
+			else {
+				volume = 0f;
 				
 				FloatControl gainControl = (FloatControl) audioClip.getControl(FloatControl.Type.MASTER_GAIN);        
 			    gainControl.setValue(20f * (float) Math.log10(volume));
